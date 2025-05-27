181d5857086c99ce03b2206d08ca4d4168e93528
aopolyakov <aopolyakov@avito.ru>
2025-05-27 23:28:27 +0300

    add tests for slices task2

--- slices/task2/private_test_cases.go
+++ slices/task2/private_test_cases.go
@@ -1,5 +1,7 @@
 package main
 
+import "math/rand"
+
 var privateTestCases = []TestCase{
 	{
 		name: "Пустой стек - пустой",
@@ -32,6 +34,118 @@ var privateTestCases = []TestCase{
 			values := stack.Values()
 			values[0] = 0
 			return compareSliceValues(stack.Values(), []int{2, 42})
+		},
+	},
+	{
+		name: "Большой стек с рандомными данными",
+		testBody: func() *Stack {
+			s := &Stack{}
+			rand.Seed(42) // фиксируем seed для воспроизводимости
+			for i := 0; i < 10000; i++ {
+				s.Push(rand.Intn(1000))
+			}
+			return s
+		},
+		check: func(s *Stack) bool {
+			if s.Len() != 10000 {
+				return false
+			}
+
+			// Проверяем, что Max() действительно возвращает максимальное значение
+			values := s.Values()
+			maxVal := 0
+			for _, v := range values {
+				if v > maxVal {
+					maxVal = v
+				}
+			}
+			return s.Max() == maxVal
+		},
+	},
+	{
+		name: "Большой стек с возрастающими данными",
+		testBody: func() *Stack {
+			s := &Stack{}
+			for i := 0; i < 10000; i++ {
+				s.Push(i)
+			}
+			return s
+		},
+		check: func(s *Stack) bool {
+			if s.Len() != 10000 {
+				return false
+			}
+
+			values := s.Values()
+			for i := 0; i < 10000; i++ {
+				if values[i] != i {
+					return false
+				}
+			}
+			return s.Max() == 9999
+		},
+	},
+	{
+		name: "Большой стек с уменьшающимися данными",
+		testBody: func() *Stack {
+			s := &Stack{}
+			for i := 9999; i >= 0; i-- {
+				s.Push(i)
+			}
+			return s
+		},
+		check: func(s *Stack) bool {
+			if s.Len() != 10000 {
+				return false
+			}
+
+			values := s.Values()
+			for i := 0; i < 10000; i++ {
+				if values[i] != 9999-i {
+					return false
+				}
+			}
+			return s.Max() == 9999
+		},
+	},
+	{
+		name: "Много операций удаления данных",
+		testBody: func() *Stack {
+			s := &Stack{}
+			for i := 0; i < 100; i++ {
+				s.Push(i)
+			}
+			for i := 0; i < 50; i++ {
+				s.Pop()
+			}
+			return s
+		},
+		check: func(s *Stack) bool {
+			if s.Len() != 50 {
+				return false
+			}
+
+			values := s.Values()
+			for i := 0; i < 50; i++ {
+				if values[i] != i {
+					return false
+				}
+			}
+			return s.Max() == 49
+		},
+	},
+	{
+		name: "Провеврка максимального значения в стеке с отрицательными числами",
+		testBody: func() *Stack {
+			stack := Stack{}
+			stack.Push(-1)
+			stack.Push(-2)
+			stack.Push(-3)
+
+			return &stack
+		},
+		check: func(stack *Stack) bool {
+			return stack.Max() == -1
 		},
 	},
 }
--- slices/task2/public_test_cases.go
+++ slices/task2/public_test_cases.go
@@ -58,15 +58,26 @@ var testCases = []TestCase{
 		},
 		check: func(stack *Stack) bool {
 			values := stack.Values()
-			for i := range values {
-				if values[len(values)-1-i] != stack.Pop() {
-					return false
-				}
-			}
-			return true
+			return values[0] == 1 && values[1] == 2
 		},
 	},
 	// Тесткейсы в помощь
+	{
+		name: "Проверяем, что элементы добавляются в конец",
+		testBody: func() *Stack {
+			stack := Stack{}
+			stack.Push(42)
+			stack.Push(2)
+			stack.Push(100)
+
+			return &stack
+		},
+		check: func(stack *Stack) bool {
+			popValue := stack.Pop()
+
+			return popValue == 100 && compareSliceValues([]int{42, 2}, stack.Values())
+		},
+	},
 	{
 		name: "Максимальный элемент самый первый",
 		testBody: func() *Stack {
